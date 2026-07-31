82475f4b27b67568cfa45c157424436ddee3598c
Radhika Borigam <164788337+Radhika-borigam@users.noreply.github.com>
2026-08-01 01:16:51 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left=0;
+        int right=people.length-1;
+        int boat=0;
+        while(left<=right){
+            if(people[right]+people[left]>limit){
+                boat++;
+                right--;
+            }else{
+                boat++;
+                left++;
+                right--;
+            }
+            
+        }
+        return boat;
+        
+    }
+}
