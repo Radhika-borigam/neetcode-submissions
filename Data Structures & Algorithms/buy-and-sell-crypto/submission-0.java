49263698527b8dd88a3afcf506dfc1b8c571a8c8
Radhika Borigam <164788337+Radhika-borigam@users.noreply.github.com>
2026-08-01 02:10:30 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int buy = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            // If we find a cheaper buying price
+            buy = Math.min(buy, prices[i]);
+
+            // Calculate today's profit
+            int profit = prices[i] - buy;
+
+            maxProfit = Math.max(maxProfit, profit);
+        }
+
+        return maxProfit;
+    }
+}
